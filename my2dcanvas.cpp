7ce68be980d69d76811fd60bd31d250ce4ca7117
stardust <open-src@qq.com>
2026-07-16 17:29:33 +0800

    提交my2dcanvas.h和.cpp文件的修改,完善头文件,实现画布缩放/视图重置接口,限制视图最大最小值,以及变化区间和变化梯度为五

---
+++ my2dcanvas.cpp
@@ -0,0 +1,113 @@
+// File: my2dcanvas.cpp
+// Created: huangyulin2024051604104 389930006@qq.com      2026-06-22
+// Version: 1.0      License: AGPLv3
+
+#include "my2dcanvas.h"
+#include "my2dcanvasrenderer.h"
+
+My2DCanvas::My2DCanvas(QQuickItem *parent)
+    : QCanvasPainterItem(parent)
+{}
+
+QCanvasPainterItemRenderer *My2DCanvas::createItemRenderer() const
+{
+    return new My2DCanvasRenderer();
+}
+
+
+
+void My2DCanvas::zoomIn()
+{
+    double cx = (m_xMin + m_xMax) / 2;
+    double cy = (m_yMin + m_yMax) / 2;
+    double xRange = (m_xMax - m_xMin) * 0.8;
+    double yRange = (m_yMax - m_yMin) * 0.8;
+
+    if (xRange < MIN_RANGE || yRange < MIN_RANGE) {
+        return;
+    }
+
+    if (xRange > MAX_RANGE) {
+        xRange = MAX_RANGE;
+    }
+    if (yRange > MAX_RANGE) {
+        yRange = MAX_RANGE;
+    }
+    setXMin(cx - xRange / 2);
+    setXMax(cx + xRange / 2);
+    setYMin(cy - yRange / 2);
+    setYMax(cy + yRange / 2);
+}
+
+void My2DCanvas::zoomOut()
+{
+    double cx = (m_xMin + m_xMax) / 2;
+    double cy = (m_yMin + m_yMax) / 2;
+    double xRange = (m_xMax - m_xMin) * 1.25;
+    double yRange = (m_yMax - m_yMin) * 1.25;
+
+    if (xRange > MAX_RANGE) {
+        xRange = MAX_RANGE;
+    }
+    if (yRange > MAX_RANGE) {
+        yRange = MAX_RANGE;
+    }
+
+    setXMin(cx - xRange / 2);
+    setXMax(cx + xRange / 2);
+    setYMin(cy - yRange / 2);
+    setYMax(cy + yRange / 2);
+}
+//坐标轴范围变大变小的区间（可更改区间，不同的区间会有不同的展示效果）
+void My2DCanvas::resetView()
+{
+    setXMin(-5);
+    setXMax(5);
+    setYMin(-5);
+    setYMax(5);
+}
+void My2DCanvas::setMultiFunctionData(const QList<QVariant> &functionsData)
+{
+    m_multiFunctionData = functionsData;
+    update();
+}
+QList<QVariant> My2DCanvas::multiFunctionData() const
+{
+    return m_multiFunctionData;
+}
+
+double My2DCanvas::xMax() const { return m_xMax; }
+void My2DCanvas::setXMax(double newXMax)
+{
+    if (qFuzzyCompare(m_xMax, newXMax)) return;
+    m_xMax = newXMax;
+    emit rangeChanged();
+    update();
+}
+
+double My2DCanvas::xMin() const { return m_xMin; }
+void My2DCanvas::setXMin(double newXMin)
+{
+    if (qFuzzyCompare(m_xMin, newXMin)) return;
+    m_xMin = newXMin;
+    emit rangeChanged();
+    update();
+}
+
+double My2DCanvas::yMin() const { return m_yMin; }
+void My2DCanvas::setYMin(double newYMin)
+{
+    if (qFuzzyCompare(m_yMin, newYMin)) return;
+    m_yMin = newYMin;
+    emit rangeChanged();
+    update();
+}
+
+double My2DCanvas::yMax() const { return m_yMax; }
+void My2DCanvas::setYMax(double newYMax)
+{
+    if (qFuzzyCompare(m_yMax, newYMax)) return;
+    m_yMax = newYMax;
+    emit rangeChanged();
+    update();
+}
---
+++ my2dcanvas.h
@@ -0,0 +1,54 @@
+// File: my2dcanvas.h
+// Created: huangyulin2024051604104 389930006@qq.com      2026-06-22
+// Version: 1.0      License: AGPLv3
+#pragma once
+
+#include <QtCanvasPainter>
+#include <QCanvasPainterItem>
+#include <QtQml/qqmlregistration.h>
+#include <QVariant>
+
+class My2DCanvas : public QCanvasPainterItem
+{
+    Q_OBJECT
+    QML_ELEMENT
+    Q_PROPERTY(double xMin READ xMin WRITE setXMin NOTIFY rangeChanged)
+    Q_PROPERTY(double xMax READ xMax WRITE setXMax NOTIFY rangeChanged)
+    Q_PROPERTY(double yMin READ yMin WRITE setYMin NOTIFY rangeChanged)
+    Q_PROPERTY(double yMax READ yMax WRITE setYMax NOTIFY rangeChanged)
+
+public:
+    explicit My2DCanvas(QQuickItem *parent = nullptr);
+
+    Q_INVOKABLE void zoomIn();
+    Q_INVOKABLE void zoomOut();
+    Q_INVOKABLE void resetView();
+
+    Q_INVOKABLE void setMultiFunctionData(const QList<QVariant> &functionsData);
+    static constexpr double MAX_RANGE = 1000;
+    static constexpr double MIN_RANGE = 0.01;
+
+    QList<QVariant> multiFunctionData() const;
+
+    double xMax() const;
+    void setXMax(double newXMax);
+    double xMin() const;
+    void setXMin(double newXMin);
+    double yMin() const;
+    void setYMin(double newYMin);
+    double yMax() const;
+    void setYMax(double newYMax);
+
+signals:
+    void rangeChanged();
+
+protected:
+    QCanvasPainterItemRenderer *createItemRenderer() const override;
+
+private:
+    double m_xMin;
+    double m_xMax;
+    double m_yMin;
+    double m_yMax;
+    QList<QVariant> m_multiFunctionData;
+};
